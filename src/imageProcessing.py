ccad8923725e842f056c4cf7ad08af9d3252ecb7
Nino Padrutt <nino.padrutt@hotmail.com>
2017-11-16 10:17:32 +0100

    some small adjustments

--- src/imageProcessing.py
+++ src/imageProcessing.py
@@ -2,6 +2,7 @@
 
 # Python 2/3 compatibility
 import sys
+
 PY3 = sys.version_info[0] == 3
 
 if PY3:
@@ -12,8 +13,9 @@ import cv2
 
 
 def angle_cos(p0, p1, p2):
-    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
-    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+    d1, d2 = (p0 - p1).astype('float'), (p2 - p1).astype('float')
+    return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1) * np.dot(d2, d2)))
+
 
 def find_squares(img):
     img = cv2.GaussianBlur(img, (5, 5), 0)
@@ -28,22 +30,27 @@ def find_squares(img):
             bin, contours, _hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
             for cnt in contours:
                 cnt_len = cv2.arcLength(cnt, True)
-                cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+                cnt = cv2.approxPolyDP(cnt, 0.02 * cnt_len, True)
                 if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
                     cnt = cnt.reshape(-1, 2)
-                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
+                    max_cos = np.max([angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4]) for i in xrange(4)])
                     if max_cos < 0.1:
                         squares.append(cnt)
     return squares
 
-if __name__ == '__main__':
+
+def analyze_image():
     from glob import glob
     for fn in glob('/data/pic*.png'):
         img = cv2.imread(fn)
         squares = find_squares(img)
-        cv2.drawContours( img, squares, -1, (0, 255, 0), 3 )
+        cv2.drawContours(img, squares, -1, (0, 255, 0), 3)
         cv2.imshow('squares', img)
         ch = cv2.waitKey()
         if ch == 27:
             break
+
+
+if __name__ == '__main__':
+    analyze_image()
     cv2.destroyAllWindows()
