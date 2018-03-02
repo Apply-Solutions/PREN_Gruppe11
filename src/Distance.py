3849506724ca9422c50750e6eb35de1c65eb00eb
christopherchristensen <christopher.christensen@live.com>
2018-03-02 11:07:10 +0100

    sending message to client conforms to interface definition

---
+++ src/Distance.py
@@ -0,0 +1,42 @@
+# Libraries
+import RPi.GPIO as GPIO
+import time
+
+
+def distance():
+    # GPIO Mode (BOARD / BCM)
+    GPIO.setmode(GPIO.BCM)
+
+    # set GPIO Pins
+    GPIO_TRIGGER = 18
+    GPIO_ECHO = 24
+    # set GPIO direction (IN / OUT)
+    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    start_time = time.time()
+    stop_time = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        start_time = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        stop_time = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = stop_time - start_time
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    dist = (TimeElapsed * 34300) / 2
+
+    GPIO.cleanup()
+
+    return dist
