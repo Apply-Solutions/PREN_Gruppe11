b7f0c9d07785c880c7a1621a1cf7d7b158748a96
christopherchristensen <christopher.christensen@live.com>
2018-03-09 10:09:15 +0100

    test for stepper_horizontal

--- src/tests/test_stepper_motor.py
+++ src/tests/test_stepper_motor.py
@@ -1,2 +1,10 @@
 from src import StepperHorizontal
+import threading
 
+
+def start_stepper_motor():
+    stepper_horizontal = StepperHorizontal()
+    try:
+        stepper_horizontal.start()
+    except(KeyboardInterrupt, SystemExit):
+        stepper_horizontal.running = False
