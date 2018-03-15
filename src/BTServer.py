d68e795563380f5454df33e3fce59c1133a17cb7
Viet Dao <viet.dao@jls.ch>
2018-03-15 09:55:26 +0100

    Working BTServer

--- src/BTServer.py
+++ src/BTServer.py
@@ -1,18 +1,22 @@
 from bluetooth import *
+from random import random
 import datetime
-import sys
 import start_cat
+import sys
+
+server_socket = BluetoothSocket(RFCOMM)
+client_sock = ''
 
 def send_message(message):
     client_sock.send(message)
 
+def demo_data():
+    return random() * 100
 
 def main():
-    server_socket = BluetoothSocket(RFCOMM)
     port = 8700
     status = "paused"
-    uuid = "b9263ab4-2eb9-4792-9472-b20b97f4b2e9"
-    client_sock = ''
+    uuid = "00001105-0000-1000-8000-00805f9b34fb"
 
     server_socket.bind(("", 0))
     server_socket.listen(1)
@@ -20,19 +24,20 @@ def main():
     print("Listening on port %d" % port)
 
     advertise_service( server_socket, "LaufkatzeT11",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ]
-                   )
+                       service_id = uuid,
+                       service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                       profiles = [ SERIAL_PORT_PROFILE ]
+                       )
 
-    address = server_socket.accept()
-    print("Accepted connection from ", address)
+
+    client_sock, client_info = server_socket.accept()
+    print("Accepted connection from ", client_info)
     client_sock.send("status@" + status + "#")
     while True:
         received_data = client_sock.recv(1024)  # receiveData here
 
         if received_data.strip() == "disconnect":
-            client_sock.send(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + "@dack")  # disconnects client
+            client_sock.send(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + "@dack" + "#")  # disconnects client
             received_data = ""
             client_sock.close()
             sys.exit("Received disconnect message. Shutting down server.")
@@ -41,9 +46,14 @@ def main():
             print("Received message from client: " + received_data)
             received_data = ""
             # function call here
-            start_cat.main()
+            start_cat.start_cat(0)
             status = "started"
-            client_sock.send(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + "status@" + status)
+
+            x = demo_data()
+            y = demo_data()
+            print(client_sock)
+            client_sock.send(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + "@Position;" + str(x) + ";" + str(y) + "#")
+            # client_sock.send("HELLO")
 
 if __name__ == '__main__':
     main()
