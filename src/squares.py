111f10fb01c36ce33fc3b160d15f8a8f6e3043df
Nino Padrutt <nino.padrutt@hotmail.com>
2018-03-16 12:24:48 +0100

    some tests to improve speed.

--- src/squares.py
+++ src/squares.py
@@ -3,9 +3,11 @@
 # Python 2/3 compatibility
 import sys
 import numpy as np
-import cv2 as cv
-
-from matplotlib import pyplot as plt
+import cv2
+import time
+import picamera
+import io
+from PIL import Image
 
 
 def angle_cos(p0, p1, p2):
@@ -16,13 +18,13 @@ def angle_cos(p0, p1, p2):
 def find_squares(img):
     squares = []
 
-    for gray in cv.split(img):
-        _retval, bin = cv.threshold(gray, 50, 255, cv.THRESH_BINARY)
-        bin, contours, _hierarchy = cv.findContours(bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for gray in cv2.split(img):
+        _retval, bin = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+        bin, contours, _hierarchy = cv2.findContours(bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
-            cnt_len = cv.arcLength(cnt, True)
-            cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True)
-            if len(cnt) == 4 and 1000 < cv.contourArea(cnt) < 70000 and cv.isContourConvex(cnt):
+            cnt_len = cv2.arcLength(cnt, True)
+            cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+            if len(cnt) == 4 and 1000 < cv2.contourArea(cnt) < 70000 and cv2.isContourConvex(cnt):
                 cnt = cnt.reshape(-1, 2)
                 max_cos = np.max([angle_cos(cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
                 if max_cos < 0.1:
@@ -33,12 +35,37 @@ def find_squares(img):
 
 if __name__ == '__main__':
     from glob import glob
-    for fn in glob('../data/pic*.jpg'):
-        img = cv.imread(fn)
-        squares = find_squares(img)
-        cv.drawContours(img, squares, -1, (0, 255, 0), 3 )
-        cv.imshow('squares', img)
-        ch = cv.waitKey()
-        if ch == 27:
-            break
-    cv.destroyAllWindows()
+
+    #for fn in glob('../data/pic*.jpg'):
+    #   img = cv.imread(fn)
+    #    squares = find_squares(img)
+    #    cv.drawContours(img, squares, -1, (0, 255, 0), 3)
+    #    cv.imshow('squares', img)
+    #    ch = cv.waitKey()
+    #    if ch == 27:
+    #        break
+    #cv.destroyAllWindows()
+
+    print(time.strftime("%d.%m.%Y %H:%M:%S"))
+    camera = picamera.PiCamera()
+    camera.resolution = (320, 240)
+    image_stream = io.BytesIO()
+    counter = 0
+    while counter < 30:
+        camera.capture(image_stream, format='jpeg', resize=(320, 240))
+        data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
+        image = cv2.imdecode(data, 1)
+
+        listOfSquares = find_squares(image);
+
+        if len(listOfSquares) == 0:
+            print("No Square found.")
+        else:
+            print(str(len(listOfSquares)) + " Squares found!!")
+
+        counter = counter +1
+
+    #cv.destroyAllWindows()
+    image_stream.close()
+    print(time.strftime("%d.%m.%Y %H:%M:%S"))
+
