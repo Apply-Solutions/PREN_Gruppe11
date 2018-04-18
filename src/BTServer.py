1a6b5023fcb3614caa13225256531689d9ba73ca
Viet Dao <viet.dao@jls.ch>
2018-04-18 15:25:52 +0200

    Updated BTServer and BTClient Working test script

--- src/BTServer.py
+++ src/BTServer.py
@@ -6,11 +6,12 @@ import sys
 import threading
 
 server_socket = BluetoothSocket(RFCOMM)
-client_sock = ''
 
 
 class BluetoothServer(threading.Thread):
     _states = ['initialised', 'searching', 'connecting', 'connected', 'waiting', 'running', 'stopped']
+
+    __client_sock__ = ''
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -19,16 +20,25 @@ class BluetoothServer(threading.Thread):
         print("Bluetooth server started")
 
     def send_message(self, message):
-        client_sock.send(message)
+        self.__client_sock__.send(message)
 
     def demo_data(self):
-        return random() * 100
+        return str(random() * 100)
 
     def getDatetime(self):
         return datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
     def get_sm(self):
         return self.sm
+
+    def server_got_signal(self):
+        print("Server received start signal")
+
+        while self.is_running():
+            data = self.getDatetime() + "@position;" + self.demo_data() + ";" + self.demo_data() + "#"
+            print(data)
+
+            self.__client_sock__.send(data)
 
     def run(self):
         port = 8700
@@ -51,25 +61,25 @@ class BluetoothServer(threading.Thread):
                           profiles=[SERIAL_PORT_PROFILE])
 
         # Wait for incoming connection
-        client_sock, client_info = server_socket.accept()
+        self.__client_sock__, client_info = server_socket.accept()
 
         # Switch state to connect and afterwards to connected
         self.connect()
         self.connected()
 
         print("Accepted connection from ", client_info)
-        client_sock.send("status@" + status + "#")
+        self.__client_sock__.send("status@" + status + "#")
 
         # Switch state to waiting_for_start_signal
         self.wait_for_start_signal()
 
-        while self.is_wait_for_start_signal() or self.is_running():
-            received_data = client_sock.recv(1024)  # receiveData here
+        while self.is_waiting() or self.is_running():
+            received_data = self.__client_sock__.recv(1024)  # receiveData here
 
             if received_data.strip() == "disconnect":
-                client_sock.send(self.getDatetime() + "@dack" + "#")  # disconnects client
+                self.__client_sock__.send(self.getDatetime() + "@dack" + "#")  # disconnects client
                 received_data = ""
-                client_sock.close()
+                self.__client_sock__.close()
                 self.stop_server()
                 sys.exit("Received disconnect message. Shutting down server.")
 
