df395a8961f42ccea6cda10f81e92e4fd38efa97
Viet Dao <viet.dao@jls.ch>
2018-04-19 16:19:19 +0200

    Renamed test.py to SimpleBTTest.py Dynamically add callback methods to object

--- src/SimpleBTTest.py
+++ src/SimpleBTTest.py
@@ -1,9 +1,11 @@
 from BTServer import BluetoothServer
 
+
 def add_btserver_transitions(machine):
     machine.add_transition(trigger='search',
                            source='initialised',
-                           dest='searching')
+                           dest='searching',
+                           after='test')
     machine.add_transition(trigger='connect',
                            source='searching',
                            dest='connecting')
@@ -16,13 +18,23 @@ def add_btserver_transitions(machine):
     machine.add_transition(trigger='start_machine',
                            source='waiting',
                            dest='running',
-                           after='server_got_signal')
+                           after='test.server_got_signal')
     machine.add_transition(trigger='stop_server',
                            source='*',
                            dest='stopped')
 
 
-if __name__=='__main__':
+def test():
+    print("Server idling until connection request")
+
+
+def server_got_signal():
+    print("Server received start signal")
+
+
+if __name__ == '__main__':
     btserver = BluetoothServer()
     add_btserver_transitions(btserver.get_sm())
+    btserver.test = test
+    btserver.server_got_signal = server_got_signal
     btserver.start()
