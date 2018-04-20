3554332edfdf00526c2408305cbd717996281ac1
christopherchristensen <christopher.christensen@live.com>
2018-04-20 11:35:18 +0200

    4 directional scripts for interpolation tests with arguments

---
+++ src/SpecificStepperH.py
@@ -0,0 +1,89 @@
+from time import sleep
+from StateMachine import StateMachine
+import math
+import RPi.GPIO as GPIO
+import threading
+
+DIR = 24  # Direction GPIO Pin
+STEP = 23  # Step GPIO Pin
+CW = 1  # Clockwise Rotation
+CCW = 0  # Counterclockwise Rotation
+SPR = 1000  # Steps per Revolution (360 / 7.5)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+
+# set direction forward (clockwise)
+GPIO.output(DIR, CCW)
+
+step_count = SPR
+delay = .0005
+
+
+class StepperH(threading.Thread):
+    _states = ['initialised', 'running_forwards', 'running_backwards', 'stopped']
+    position = [0, 0]
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.steps = 0;
+        self.delay = 0.05
+        self.count = 5
+        self.sm = StateMachine.get_stepperh_machine(self, StepperH._states)
+
+    def run(self):
+        print("\nStepperH ON")
+
+        # TODO: Change to actual pos
+        while self.is_running_forwards() and self.get_x() < 20:
+            self.do_steps()
+
+        self.clean_up()
+
+        print("[StepperH]: Waiting for state change")
+        while self.is_stopped():
+            pass
+
+        while self.is_running_forwards():
+            self.do_steps()
+        self.clean_up()
+
+    def get_sm(self):
+        return self.sm
+
+    def do_steps(self):
+        if self.delay > 0.0005:
+            self.delay = math.exp(-self.count) + 0.0005
+            self.count = self.count + 0.02
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(DIR, GPIO.OUT)
+        GPIO.setup(STEP, GPIO.OUT)
+
+        self.steps = self.steps + 1
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(self.delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(self.delay)
+        self.update_position()
+        print(self.steps)
+
+    def update_position(self):
+        self.position[0] += 1
+        self.position[1] += 1
+
+    def get_x(self):
+        return self.position[0]
+
+    def get_y(self):
+        return self.position[1]
+
+    @staticmethod
+    def set_direction(direction):
+        GPIO.output(DIR, direction)
+
+    @staticmethod
+    def clean_up():
+        print("\nStepperH OFF")
+        GPIO.cleanup()
